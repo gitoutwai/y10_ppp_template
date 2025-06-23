9abf8cd52618413dd13024e61737e35d83604ae9
gitoutwai <wai@kellettschool.com>
2025-06-23 12:16:17 +0800

    Created shell types with different damage caused

--- shells.py
+++ shells.py
@@ -1,11 +1,41 @@
 from random import randint
+from random import choice
 
 class Shell:
-    def __init__(self, name, type, calibre):
-        self.name = name
+    def __init__(self, type, calibre):
         self.type = type
         self.calibre = calibre
 
-        def damage(self):
-            if self.type == "APCBC":
-                damage = randint(200, 500)
+    def damage(self):
+        if self.type == "APCBC":
+            hittypes = choice(["direct hit", "direct hit", "ricochet", "miss", "non-penetrate"])
+            if hittypes == "direct hit":
+                damage = (randint(200, 400) * (self.calibre/10))
+            elif hittypes == "ricochet":
+                damage = (randint(0, 50) * (self.calibre/10))
+            elif hittypes == "miss":
+                damage = 0
+            elif hittypes == "non-penetrate":
+                damage = (randint(0, 100) * (self.calibre/10))
+        elif self.type == "HE":
+            hittypes = choice(["direct hit", "ricochet", "miss", "non-penetrate", "non-penetrate", "non-penetrate"])
+            if hittypes == "direct hit":
+                damage = (randint(0, 100) * (self.calibre/10))
+            elif hittypes == "ricochet":
+                damage = (randint(0, 5) * (self.calibre/10))
+            elif hittypes == "miss":
+                damage = 0
+            elif hittypes == "non-penetrate":
+                damage = (randint(0, 10) * (self.calibre/10))
+        elif self.type == "APHE":
+            hittypes = choice(["direct hit", "ricochet", "miss", "non-penetrate"])
+            if hittypes == "direct hit":
+                damage = (randint(300, 500) * (self.calibre/10))
+            elif hittypes == "ricochet":
+                damage = (randint(0, 100) * (self.calibre/10))
+            elif hittypes == "miss":
+                damage = 0
+            elif hittypes == "non-penetrate":
+                damage = (randint(0, 200) * (self.calibre/10))
+
+        return damage
